73f61024646fc086a87dea740c5f46b630d21257
Dennis Hu <74347437+Dennis-who@users.noreply.github.com>
2023-12-04 18:31:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Define your vocabulary list
+const vocabularyList = [
+    "affiliate", "bypass", "rigorous", "solicitation", "delicate", "obstruct",
+    "recreational", "fabrication", "tamper", "conceptualize", "solicited",
+    "by virtue of", "genocide", "speculate", "articulate", "artisan", "cluster",
+    "mitigate", "substantiate", "millennial", "fracture", "inception", "phishing",
+    "vishing", "smishing", "encrypted", "ransom", "solicit", "elicited", "manipulate",
+    "tension", "daunting", "tactic", "credibility", "meditation", "mindfulness",
+    "endorse", "confrontation", "gossip", "traumatized", "empirical", "synapses",
+    "temper", "twig", "synopsis", "sanitize", "fluff", "flesh out", "extrapolate",
+    "cynical", "embark", "edamame", "beet", "eel", "seaweed", "bamboo shoot",
+    "scallion", "kimchee", "broth", "tonkotsu", "distasteful", "crummy",
+    "tailgating party", "nuance", "scrutinize", "somatic", "dissonance", "sanction",
+    "censure", "tsunami", "displacement", "cease", "static", "dynamic", "humility",
+    "hegemony", "doodle", "feasibility", "pragmatic", "extensive", "deployment",
+    "nomination", "feudalism", "provocative", "testimonial", "irrevocably",
+    "pro bono", "jarring", "executive summary", "tangible", "expatriates", "flyer",
+    "incentive", "bequest", "capacity", "exacerbate", "disrupt", "irascible",
+    "procrastination", "corny", "cheesy", "enviable", "recession", "entity",
+    "bankrupt", "encapsulate", "recipient", "misnomer", "devastating", "amendment"
+];
+
+let currentIndex = 0;
+
+// Function to shuffle an array using the Fisher-Yates algorithm
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+// Shuffle the vocabulary list initially
+shuffleArray(vocabularyList);
+
+// Function to display a random word
+function showNextWord() {
+    const flashcardWord = document.getElementById("flashcard-word");
+
+    // Shuffle the vocabulary list if all words have been displayed
+    if (currentIndex >= vocabularyList.length) {
+        shuffleArray(vocabularyList);
+        currentIndex = 0;
+    }
+
+    // Display the current word
+    flashcardWord.textContent = vocabularyList[currentIndex];
+    currentIndex++;
+}
